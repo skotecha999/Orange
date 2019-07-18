a2edcfa0723586aebac1c9e1055b19743c22ca3d
vishwas <vishwassadashivaiah15@gmail.com>
2019-07-17 23:55:21 -0700

    signupform

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },  { path: 'signuppage', loadChildren: './signuppage/signuppage.module#SignuppagePageModule' },
+
 ];
 
 @NgModule({
