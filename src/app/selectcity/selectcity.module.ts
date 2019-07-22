09793575a226acd876ee59bd8a5e8c6d5430642d
vishwadeep-tejaswi <vishwadeep.tejaswi@gmail.com>
2019-07-22 15:11:52 +0530

    fail

--- src/app/selectcity/selectcity.module.ts
+++ src/app/selectcity/selectcity.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 
 import { SelectcityPage } from './selectcity.page';
 
@@ -23,4 +23,25 @@ const routes: Routes = [
   ],
   declarations: [SelectcityPage]
 })
-export class SelectcityPageModule {}
+
+
+
+export class SelectcityPageModule { 
+  auto(event){
+    const searchbar = document.querySelector('ion-searchbar');
+    const items = Array.from(document.querySelector('ion-list').children);
+    searchbar.addEventListener('ionInput', handleInput);
+    function handleInput(event) {
+      const query = event.target.value.toLowerCase();
+      requestAnimationFrame(() => {
+        items.forEach(item => {
+          const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
+          item.style.display = shouldShow ? 'block' : 'none';
+        });
+      });
+    }
+  }
+ 
+}
+
+
