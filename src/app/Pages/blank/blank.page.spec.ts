0ce6c1ce0e1fe5ee1f07f3baed7dda07f63a75c1
Umesh S M <umeshsmg007@gmail.com>
2019-07-25 13:11:41 +0530

    loginmodulerestore

--- src/app/Pages/blank/blank.page.spec.ts
+++ src/app/Pages/blank/blank.page.spec.ts
@@ -1,6 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BlankPage } from './blank.page';
 
 describe('BlankPage', () => {
