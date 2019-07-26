7ba07ea45f1e66cdee6713fe7781144db1fe5530
Umesh S M <umeshsmg007@gmail.com>
2019-07-26 16:30:08 +0530

    121

---
+++ src/app/Components/menu/menu.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+})
+export class MenuComponent implements OnInit {
+
+  
+  selectedPath = '';
+pages = [
+    {
+      title: 'Home',
+    },
+    {
+      title: 'My Subscriptions'
+    },
+    {
+      title: 'Settings',
+    },
+    {
+      title: 'Help',
+    }
+  ];
+
+  calender=['7-july','8-july','9-july'];
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
+
--- src/app/Pages/userhome/userhome.module.ts
+++ src/app/Pages/userhome/userhome.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-
 import { UserhomePage } from './userhome.page';
-import {ScrollDispatchModule} from '@angular/cdk/scrolling';
+import { ScrollDispatchModule } from '@angular/cdk/scrolling';
 import { FilterComponent } from 'src/app/Components/filter/filter.component';
 import { FilterpagePage } from '../filterpage/filterpage.page';
-
+import { MenuComponent } from 'src/app/Components/menu/menu.component';
 
 const routes: Routes = [
   {
@@ -25,8 +24,8 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     ScrollDispatchModule
   ],
-  declarations: [UserhomePage, FilterComponent, FilterpagePage],
-  entryComponents: [FilterpagePage]
+  declarations: [UserhomePage, FilterComponent, FilterpagePage, MenuComponent],
+  entryComponents: [FilterpagePage,  MenuComponent]
 })
 export class UserhomePageModule {}
 
