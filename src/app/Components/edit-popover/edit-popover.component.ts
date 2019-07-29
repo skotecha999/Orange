217f95cf779f01abfb726a72b35917433a1a57b3
skotecha999 <skotecha999@gmail.com>
2019-07-29 15:26:46 +0530

    pop

---
+++ src/app/Components/edit-popover/edit-popover.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-edit-popover',
+  templateUrl: './edit-popover.component.html',
+  styleUrls: ['./edit-popover.component.scss'],
+})
+export class EditPopoverComponent implements OnInit {
+@Input() data: {title:string, text:string};
+  constructor(public popoverController: PopoverController) { }
+
+  ngOnInit() {}
+
+}
